085a4165eacb9cae87f96314ace993ffc398e2b3
BBee <atsushi.kokubun.47@gmail.com>
2026-04-04 22:43:15 +0900

    Fix syntax in main.js and spring boot test code

--- src/test/java/jp/developer/bbee/pcassem/PcassemApplicationTests.java
+++ src/test/java/jp/developer/bbee/pcassem/PcassemApplicationTests.java
@@ -3,11 +3,19 @@ package jp.developer.bbee.pcassem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"gemini.api.key=dummy",
+		"firebase.project.id=dummy-project",
+		"firebase.web.api-key=dummy",
+		"firebase.web.auth-domain=dummy.firebaseapp.com",
+		"spring.main.lazy-initialization=true"
+})
 class PcassemApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
